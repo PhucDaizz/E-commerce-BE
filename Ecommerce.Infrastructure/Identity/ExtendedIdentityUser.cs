cd2764f4d1c88ebbfb522e46e2384b88912f88b7
Phúc Đại <dai742004.dn@gmail.com>
2025-07-31 16:06:26 +0700

    feat: Setup ApplicationDbContext and EF Core configurations

---
+++ Ecommerce.Infrastructure/Identity/ExtendedIdentityUser.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Identity
+{
+    public class ExtendedIdentityUser : IdentityUser
+    {
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiry { get; set; }
+        public bool? Gender { get; set; }
+        public string? Address { get; set; }
+
+        // Navigation properties
+        public ICollection<Conversations> ClientConversations { get; set; } = new List<Conversations>();
+        public ICollection<Conversations> AdminAssignedConversations { get; set; } = new List<Conversations>();
+        public ICollection<ChatMessage> SentMessages { get; set; } = new List<ChatMessage>();
+    }
+}
